b6a33216422ab4a905e1ef0a87571684d4863ddc
naveen1555 <kasukurthinaveen11@gmail.com>
2022-09-20 05:16:02 +0000

    firstCommit

---
+++ src/components/PlanetItem/index.js
@@ -0,0 +1,17 @@
+import './index.css'
+
+const PlanetItem = props => {
+  const {planetDetails} = props
+
+  const {name, imageUrl, description} = planetDetails
+
+  return (
+    <div className="slide-container">
+      <img src={imageUrl} alt={`planet ${name}`} className="planet-image" />
+      <h1 className="planet-name">{name}</h1>
+      <p className="description">{description}</p>
+    </div>
+  )
+}
+
+export default PlanetItem
